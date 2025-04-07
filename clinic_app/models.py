79c46cd8bcce276eaa3abdc1841ce927968679a2
Alyona <alymkan23@gmail.comgit config --global user.name >
2025-04-07 13:09:56 +0600

    Добавила финальные корректировки в Модели

--- clinic_app/models.py
+++ clinic_app/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from django.utils.version import version_component_re
 
 
-class Schedule(models.Model):
-    work_days = models.TextField(verbose_name='Рабочие дни')
-    rest_days = models.TextField(verbose_name='Выходные дни')
+class Schedules(models.Model):
+    work_days = models.TextField(max_length=50, verbose_name='Рабочие дни')
+    rest_days = models.TextField(max_length=50, verbose_name='Выходные дни')
     start_time = models.DateTimeField(verbose_name='Начало работы')
     end_time = models.DateTimeField(verbose_name='Конец работы')
     is_working = models.BooleanField(default=True)
 
 class Services(models.Model):
     name = models.CharField(max_length=50,  verbose_name='Название услуги')
-    description = models.TextField(verbose_name='Описание услуги')
-    price = models.FloatField(verbose_name='Цена')
+    description = models.TextField(max_length=300, verbose_name='Описание услуги')
+    price = models.FloatField(blank=True, null=True, verbose_name='Цена')
     #doctor =
 
 class Categories(models.Model):
-    name = models.CharField(max_length=70, verbose_name='Название категории')
-    description = models.TextField(verbose_name='Описании категории')
-    image = models.ImageField(verbose_name='фото категории')
+    name = models.CharField(max_length=100, verbose_name='Название категории')
+    description = models.TextField(max_length=500, verbose_name='Описании категории')
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='фото категории')
 
-class Review(models.Model):
+class Reviews(models.Model):
     REVIEW_CHOICES = [
         ('bad', 'плохо'),
         ('good', 'хорошо'),
@@ -35,15 +35,18 @@ class Review(models.Model):
 class Clinics(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(max_length=200, verbose_name = 'Описание')
-    location = models.TextField(max_length=50, verbose_name='Адрес')
+    location = models.TextField(max_length=100, verbose_name='Адрес')
     contact = models.IntegerField(verbose_name='Контакты')
-    image = models.ImageField(verbose_name='Фото')
-    logo = models.ImageField(verbose_name='Лого')
+    image = models.ImageField(upload_to = 'images/', null=True, blank=True, verbose_name='Фото')
+    logo = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Лого')
     #doctors =
-    rating = models.ForeignKey(Review, on_delete=models.CASCADE, verbose_name='Рейтинг')
+    rating = models.ForeignKey(Reviews, on_delete=models.CASCADE, verbose_name='Рейтинг')
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='Категории')
     services = models.ForeignKey(Services, on_delete=models.CASCADE, verbose_name='Услуги')
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, verbose_name='График работы')
-    created_at = models.DateField(verbose_name='Дата создания')
+    schedule = models.ForeignKey(Schedules, on_delete=models.CASCADE, verbose_name='График работы')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
+    class Meta:
+        verbose_name = 'Клиника'
+        verbose_name_plural = 'Клиники'
 
